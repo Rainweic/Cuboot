c76cca5d372dadb40bb724a0eb3046984d55c670
cu <luqin.li@lkcoffee.com>
2024-12-27 14:56:08 +0800

    update

--- driver/oled/src/oled.rs
+++ driver/oled/src/oled.rs
@@ -53,31 +53,31 @@ impl OLED {
         oled.write_cmd(0xD5);   // 设置时钟分频因子,震荡频率
         oled.write_cmd(80);     // [3:0],分频因子;[7:4],震荡频率
         oled.write_cmd(0xA8);   // 设置驱动路数
-        oled.write_cmd(0x3F);   //默认0X3F(1/64) 
-        oled.write_cmd(0xD3);   //设置显示偏移
-        oled.write_cmd(0x00);   //默认为0
+        oled.write_cmd(0x3F);   // 默认0X3F(1/64) 
+        oled.write_cmd(0xD3);   // 设置显示偏移
+        oled.write_cmd(0x00);   // 默认为0
 
-        oled.write_cmd(0x40);   //设置显示开始行 [5:0],行数.
+        oled.write_cmd(0x40);   // 设置显示开始行 [5:0],行数.
 														
-        oled.write_cmd(0x8D);   //电荷泵设置
-        oled.write_cmd(0x14);   //bit2，开启/关闭
-        oled.write_cmd(0x20);   //设置内存地址模式
-        oled.write_cmd(0x02);   //[1:0],00，列地址模式;01，行地址模式;10,页地址模式;默认10;
-        oled.write_cmd(0xA1);   //段重定义设置,bit0:0,0->0;1,0->127;
-        oled.write_cmd(0xC0);   //设置COM扫描方向;bit3:0,普通模式;1,重定义模式 COM[N-1]->COM0;N:驱动路数
-        oled.write_cmd(0xDA);   //设置COM硬件引脚配置
-        oled.write_cmd(0x12);   //[5:4]配置
+        oled.write_cmd(0x8D);   // 电荷泵设置
+        oled.write_cmd(0x14);   // bit2，开启/关闭
+        oled.write_cmd(0x20);   // 设置内存地址模式
+        oled.write_cmd(0x02);   // [1:0],00，列地址模式;01，行地址模式;10,页地址模式;默认10;
+        oled.write_cmd(0xA1);   // 段重定义设置,bit0:0,0->0;1,0->127;
+        oled.write_cmd(0xC0);   // 设置COM扫描方向;bit3:0,普通模式;1,重定义模式 COM[N-1]->COM0;N:驱动路数
+        oled.write_cmd(0xDA);   // 设置COM硬件引脚配置
+        oled.write_cmd(0x12);   // [5:4]配置
 		 
-        oled.write_cmd(0x81);   //对比度设置
-        oled.write_cmd(0xEF);   //1~255;默认0X7F (亮度设置,越大越亮)
-        oled.write_cmd(0xD9);   //设置预充电周期
-        oled.write_cmd(0xf1);   //[3:0],PHASE 1;[7:4],PHASE 2;
-        oled.write_cmd(0xDB);   //设置VCOMH 电压倍率
-        oled.write_cmd(0x30);   //[6:4] 000,0.65*vcc;001,0.77*vcc;011,0.83*vcc;
+        oled.write_cmd(0x81);   // 对比度设置
+        oled.write_cmd(0xEF);   // 1~255;默认0X7F (亮度设置,越大越亮)
+        oled.write_cmd(0xD9);   // 设置预充电周期
+        oled.write_cmd(0xf1);   // [3:0],PHASE 1;[7:4],PHASE 2;
+        oled.write_cmd(0xDB);   // 设置VCOMH 电压倍率
+        oled.write_cmd(0x30);   // [6:4] 000,0.65*vcc;001,0.77*vcc;011,0.83*vcc;
         
-        oled.write_cmd(0xA4);   //全局显示开启;bit0:1,开启;0,关闭;(白屏/黑屏)
-        oled.write_cmd(0xA6);   //设置显示方式;bit0:1,反相显示;0,正常显示	    						   
-        oled.write_cmd(0xAF);   //开启显示	
+        oled.write_cmd(0xA4);   // 全局显示开启;bit0:1,开启;0,关闭;(白屏/黑屏)
+        oled.write_cmd(0xA6);   // 设置显示方式;bit0:1,反相显示;0,正常显示	    						   
+        oled.write_cmd(0xAF);   // 开启显示	
 
         oled.clear(); 
 
